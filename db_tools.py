2f167428c06b77f6a80d3266735de7b504366f94
AlexisEidelman <alexis.eidelman.pro@gmail.com>
2015-09-15 09:14:55 +0200

    input password

--- db_tools.py
+++ db_tools.py
@@ -8,7 +8,7 @@ from sqlalchemy import create_engine
 import psycopg2
 
 # cfg
-from Tools.config import *
+import Tools.config as config
 
 ##############
 # PostgreSQL #
@@ -18,9 +18,18 @@ from Tools.config import *
 # -- Connect
 def connect(schema, table_name):
     ''' connexion postgre à partir d'un tuple (schema, table) et renvoie un tuple (table, colnames)'''
-    conn_string = "host='" + ipserver + "' dbname='" + dbname + "' user='" + user + "' password='" + password + "' client_encoding='utf-8' "
+    password = input("Entrez le mot de passe du server : ")
+    dbname = input("Par défaut, la base chargée est celle de config, entrez" + \
+                    " un autre nom si vous voulez, sinon, touche entrée")
+    if dbname == '':
+        dbname = config.dbname
+
+    conn_string = "host='" + config.ipserver + "' dbname='" + dbname + \
+                   "' user='" + config.user + "' password='" + password + \
+                   "' client_encoding='utf-8' "
     # print the connection string we will use to connect
     conn = psycopg2.connect(conn_string)
+    del password, conn_string
     cur = conn.cursor()
     print("Connected!")
     command = "SELECT * FROM " + schema + "." + table_name
@@ -31,7 +40,14 @@ def connect(schema, table_name):
 
 # -- Create table
 def create_table(df, table_name, schema):
-    engine = create_engine(r'postgresql://' + user + ':' + password + '@' + ipserver + '/' + user, encoding='utf-8')
+    password = input("Entrez le mot de passe du server : ")
+
+    conn_string = r'postgresql://' + config.user + ':' + password + '@' + \
+        config.ipserver + '/' + config.user
+    engine = create_engine(conn_string, encoding='utf-8')
+
+    del password, conn_string
+
     try:
         df.to_sql(table_name, engine, schema=schema)
         print("Table créée")
