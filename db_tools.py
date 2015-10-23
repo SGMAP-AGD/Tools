90296247b868ae088a82dff6495ce1b6bec1f216
flosugar <florian.work@gmail.com>
2015-10-23 11:52:12 +0200

    ssh connect (good one)

--- db_tools.py
+++ db_tools.py
@@ -5,10 +5,10 @@ Created on Wed Sep  9 14:10:23 2015
 @author: Florian
 """
 
-import getpass
 from sqlalchemy import create_engine
 import psycopg2
-
+import paramiko
+import pandas as pd
 # cfg
 import Tools.config as config
 
@@ -19,19 +19,16 @@ import Tools.config as config
 
 # -- Connect
 def connect(schema, table_name):
-    ''' connexion postgre à partir d'un tuple (schema, table) et renvoie un tuple (table, colnames)'''
-    password = getpass.getpass("Entrez le mot de passe du server : ")
-    dbname = input("Par défaut, la base chargée est celle de config, entrez" +
-                   " un autre nom si vous voulez, sinon, touche entrée")
-    if dbname == '':
-        dbname = config.dbname
+    ''' connexion postgre à partir d'un tuple (schema, table)
+        et renvoie un tuple (table, colnames)'''
 
-    conn_string = "host='" + config.ipserver + "' dbname='" + dbname + \
-                  "' user='" + config.user + "' password='" + password + \
-                  "' client_encoding='utf-8' "
+    conn_string = "host='localhost' dbname='" + config.dbname + "'" + \
+                  "user='" + config.user + "' password=''" + \
+                  " client_encoding='utf-8' "
+
     # print the connection string we will use to connect
     conn = psycopg2.connect(conn_string)
-    del password, conn_string
+    del conn_string
     cur = conn.cursor()
     print("Connected!")
     command = "SELECT * FROM " + schema + "." + table_name
@@ -42,17 +39,36 @@ def connect(schema, table_name):
 
 # -- Create table
 def create_table(df, table_name, schema):
-    password = getpass.getpass("Entrez le mot de passe du server : ")
+    conn_string = r'postgresql://' + config.user + ':''@localhost' + \
+        '/' + config.dbname
+    engine = create_engine(conn_string, client_encoding='utf-8')
 
-    conn_string = r'postgresql://' + config.user + ':' + password + '@' + \
-        config.ipserver + '/' + config.user
-    engine = create_engine(conn_string, encoding='utf-8')
+    del conn_string
 
-    del password, conn_string
+    return df.to_sql(table_name, engine, schema=schema, if_exists='replace')
 
+#######
+# SSH #
+#######
+
+
+def connect_sftp_csv(rep, csv):
+    ''' crée une connexion sftp sur le server secure et permet notamment
+    l'accès à des fichiers contenus sur le server'''
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     try:
-        df.to_sql(table_name, engine, schema=schema)
-        print("Table créée")
-    except ValueError:
-        print("Table already exists")
-        pass
+        ssh.connect(config.ipsecure, username=config.user, password='')
+    except paramiko.SSHException:
+        print("Connection Error")
+
+    sftp = ssh.open_sftp()
+    sftp.chdir(rep)
+    table = pd.read_csv(sftp.open(csv))
+    ssh.close()
+    return table
+
+    # USE #
+    # sftp.chdir("/var/data/stsisi/")
+    # sftp.open('iris60.csv')
+    # ssh.close()
