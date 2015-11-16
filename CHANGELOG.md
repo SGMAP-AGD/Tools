0eeaa5d9cae790a9150c937aa051830879d9d98a
Michel Blancard <michel.blancard@data.gouv.fr>
2015-11-16 12:28:17 +0100

    add changelog + setup big fix

---
+++ CHANGELOG.md
@@ -0,0 +1,13 @@
+## 0.0.3
+
+* bug fix in setup.py
+
+## 0.0.2
+
+* CSV over ssh
+* postgresql wrapper
+* k-anonymity
+
+## 0.0.1
+
+Just a test
--- setup.py
+++ setup.py
@@ -21,5 +21,5 @@ setup(
                       'pandas',
                       'paramiko',
                       'sklearn',
-                      'configparser']
+                      ]
 )
