dc0e73e1a6beeaff311dc98aa163767fbbd1b139
Michel Blancard <michel.blancard@data.gouv.fr>
2015-12-04 14:32:30 +0100

    add email notifications (test on failure)

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
     classifiers=['Development Status :: 2 - Pre-Alpha',
                  'Programming Language :: Python :: 3'],
     install_requires=['numpy',
+                      'doesnotexist5489546', # create an error
                       'scipy',
                       'SQLAlchemy',
                       'psycopg2',
