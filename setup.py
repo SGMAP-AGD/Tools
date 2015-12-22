e8cc01a88d53997d56ffede407a046d3b0f6d353
Michel Blancard <michel.blancard@data.gouv.fr>
2015-12-22 20:18:01 +0100

    add a geographic module

--- setup.py
+++ setup.py
@@ -24,5 +24,6 @@ setup(
                       'pandas',
                       'paramiko',
                       'scikit-learn >= 0.17',
+                      'geographiclib',
                       ]
 )
